2c798e359279be6b01a346b0fc7aed1df8875671
lunginspector <138542747+lunginspector@users.noreply.github.com>
2025-05-27 15:35:56 -0700

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,2 +1,2 @@
-/Users/appleinc/Library/Developer/Xcode/DerivedData/Burst-dflsuppcgqqeihhctqpiuhxsdrid/Build/Products/Debug-iphoneos/Burst.app: FORCE
-        xcodebuild -scheme Burst -derivedDataPath build -destination 'generic/platform=iOS' CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO GCC_PREPROCESSOR_DEFINITIONS='NIGHTLY=1 COMMIT_HASH=\"$(COMMIT_HASH)\"'
+/Users/main/Library/Developer/Xcode/DerivedData/Bridge-exggsgamgidihghlgbqeqrqhfbyl/Build/Products/Debug-iphoneos: FORCE
+        xcodebuild -scheme Bridge -derivedDataPath build -destination 'generic/platform=iOS' CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO GCC_PREPROCESSOR_DEFINITIONS='NIGHTLY=1 COMMIT_HASH=\"$(COMMIT_HASH)\"'
