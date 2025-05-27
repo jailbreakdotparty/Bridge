e4d20c19de9aeb6e62a0df4fa9bc0002f92485fd
lunginspector <138542747+lunginspector@users.noreply.github.com>
2025-05-27 15:34:21 -0700

    Add files via upload

---
+++ prepare.sh
@@ -0,0 +1,2 @@
+/Users/appleinc/Library/Developer/Xcode/DerivedData/Burst-dflsuppcgqqeihhctqpiuhxsdrid/Build/Products/Debug-iphoneos/Burst.app: FORCE
+        xcodebuild -scheme Burst -derivedDataPath build -destination 'generic/platform=iOS' CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO GCC_PREPROCESSOR_DEFINITIONS='NIGHTLY=1 COMMIT_HASH=\"$(COMMIT_HASH)\"'
