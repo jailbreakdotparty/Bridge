120ac22797366d29f3a3434297efe9a3b9d5e1da
lunginspector <138542747+lunginspector@users.noreply.github.com>
2025-10-21 20:58:18 -0700

    Initial Commit

---
+++ Bridge/BridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BridgeApp.swift
+//  Bridge
+//
+//  Created by Main on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BridgeTests/BridgeTests.swift
@@ -0,0 +1,16 @@
+//
+//  BridgeTests.swift
+//  BridgeTests
+//
+//  Created by Main on 10/21/25.
+//
+
+import Testing
+
+struct BridgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
